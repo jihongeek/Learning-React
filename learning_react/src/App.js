fdd4cad3e835931f45cb9433518d1658f59890b2
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-05-29 10:26:40 +0000

    JSX 문법과 스타일링

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -5,9 +5,18 @@ import Footer from './component/Footer';
 
 
 function App() {
+  const appDescription = {
+    fisrtLine : `이 React 앱은 곤충🐜이 머리-가슴-배로 이루어져 있듯이,`,
+    secondLine : `Header💀-Body🦴-Footer🦶로 이루어져 있습니다.` 
+  }
+  // JSX에서는 모든 태그를 큰 최상위 태그로 감싸야 함
   return (
     <div className="App">
-      { /*self-closing tag : 항상 마지막에 /을 붙혀줘야함 
+      {/*JSX에서 자바스크립트 표현식 사용
+      (반환 값이 : number,string,boolean,null,undefined 일때 만!*/}
+      <p>{appDescription.fisrtLine}</p>
+      <p>{appDescription.secondLine}</p>
+      { /*self-closing tag(모든 태그는 닫힘 규칙 적용) : 항상 마지막에 /을 붙혀줘야함 
       Header 컴포넌트를 App 컴포넌트의 자식 컴포넌트로 '배치' */}
       <Header/>
       <Body/>
--- learning_react/src/component/Body.js
+++ learning_react/src/component/Body.js
@@ -1,10 +1,29 @@
+import "./Body.css";
+
 const Body = () => {
-    return (
-        <div>
-            <h1>My Body 🦴</h1>
-            <p>이 리액트 앱의 '몸뚱아리'랍니다.</p>
-        </div>
-    )
+    let language = "korean";
+    // 조건문을 이용한 조건부 렌더링
+    if (language === "korean")
+    {
+        return (
+            // JSX 방식으로 class 대신 className 사용
+            // style에 style 객체를 넘겨줘서 인라인 스타일링 구현
+            <div className = "body" style = {{color : "green"}}>
+                <h1>My Body 🦴</h1>
+                <p>이 리액트 앱의 '몸뚱아리'랍니다.</p>    
+            </div>
+        )
+    }
+    else
+    {
+        return (
+            <div className = "body" style = {{color : "crimson"}}>
+                <h1>My Body 🦴</h1>
+                <p>Here is 'BODY' of this React app.</p>    
+            </div>
+        )
+
+    }
 }
 
 export default Body;
