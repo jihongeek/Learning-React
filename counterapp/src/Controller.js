f6d6de9c0429ae79e4e7ee23225fd361fd977451
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-06-07 14:31:18 +0000

    카운터 앱 글옵 느낌으로 바꿈

--- counterapp/src/Controller.js
+++ counterapp/src/Controller.js
@@ -2,12 +2,12 @@
 const Controller = ({clickHandle}) => {
     return (
         <div>
-            <button onClick={clickHandle}>1-Death</button>
-            <button onClick={clickHandle}>3-Death</button>
-            <button onClick={clickHandle}>5-Death</button>
-            <button onClick={clickHandle}>5-Kill</button>
-            <button onClick={clickHandle}>5-Kill</button>
-            <button onClick={clickHandle}>1-Kill</button>
+            <button style = {{backgroundColor : "rgb(222,155,53)"}} onClick={clickHandle}>1-Death</button>
+            <button style = {{backgroundColor : "rgb(222,155,53)"}} onClick={clickHandle}>3-Death</button>
+            <button style = {{backgroundColor : "rgb(222,155,53)"}} onClick={clickHandle}>5-Death</button>
+            <button style = {{backgroundColor : "rgb(222,155,53)"}} onClick={clickHandle}>5-Kill</button>
+            <button style = {{backgroundColor : "rgb(222,155,53)"}} onClick={clickHandle}>5-Kill</button>
+            <button style = {{backgroundColor : "rgb(222,155,53)"}} onClick={clickHandle}>1-Kill</button>
         </div>
     );
 }
