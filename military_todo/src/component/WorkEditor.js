b78794fbb39ef25ac0f2d892ec4e37959b29c23a
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-06-21 14:01:18 +0000

    WorkEditor 컴포넌트 및 스타일 작성 완료

--- military_todo/src/component/WorkEditor.js
+++ military_todo/src/component/WorkEditor.js
@@ -3,6 +3,11 @@ import "./WorkEditor.css"
 const WorkEditor = () => {
     return (
         <div className = "WorkEditor">
+            <p>새로운 작업 작성하기!</p>
+            <div className = "editor_wrapper">
+                <input placeholder="하.. 새로운 작업..ㅠ"></input>
+                <button>추가</button>
+            </div>
         </div>
     );
 }
