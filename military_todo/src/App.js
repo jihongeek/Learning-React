61b697327b10d15c3e1d6aa2392d2b2e7f588b7e
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-06-22 13:50:49 +0000

    App에 WorkList 임포트하고 WorkList 컴포넌트 일단 완성

--- military_todo/src/App.js
+++ military_todo/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./component/Header";
 import WorkEditor from "./component/WorkEditor";
+import WorkList from "./component/WorkList"; 
 
 function App() {
   return (
--- military_todo/src/component/WorkList.js
+++ military_todo/src/component/WorkList.js
@@ -3,6 +3,8 @@ import "./WorkList.css"
 const WorkList = () => {
     return (
       <div className = "WorkList">
+          <p>작업목록표</p>
+          <input className = "searchbar" placeholder = "작업 검색" ></input>
       </div>  
     );
 }
