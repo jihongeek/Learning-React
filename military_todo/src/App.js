9f076cd310a94f2ec62d3c6e6c62529e2a18fb4e
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-06-25 09:50:01 +0000

    데작업목록을 위한 데이터 모델링 후, 목업 데이터 생성 후 App에 넘겨줌

--- military_todo/src/App.js
+++ military_todo/src/App.js
@@ -2,8 +2,36 @@ import "./App.css";
 import Header from "./component/Header";
 import WorkEditor from "./component/WorkEditor";
 import WorkList from "./component/WorkList"; 
+import {useState} from "react"
 
+const mockWork = [
+  {
+    id : 0,
+    name : "전방에 힘찬 함성 3초간 실시",
+    isDone: false,
+    createDate : new Date().getTime()
+  },
+  {
+    id : 1,
+    name : "애국가 제창",
+    isDone: false,
+    createDate : new Date().getTime()
+  },
+  {
+    id : 2,
+    name : "조국기도문 낭독",
+    isDone: false,
+    createDate : new Date().getTime()
+  },
+  {
+    id : 3,
+    name : "국군도수체조 실시",
+    isDone: false,
+    createDate : new Date().getTime()
+  }
+]
 function App() {
+  const [work,setWork] = useState(mockWork);
   return (
     <div className="App">
       <Header/>
