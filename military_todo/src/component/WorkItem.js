879302dc22fa93713aefdaa0831c57f581c720a0
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-07-02 10:56:10 +0000

    createDate -> createdDate 오타 수정

--- military_todo/src/component/WorkItem.js
+++ military_todo/src/component/WorkItem.js
@@ -1,12 +1,12 @@
 import "./WorkItem.css"
-const WorkItem = ({id, name, isDone, createDate}) => {
+const WorkItem = ({id, name, isDone, createdDate}) => {
     return (
         <div className = "WorkItem">
             <div className = "checkbox_col">
                 <input type = "checkbox" checked = {isDone}/>
             </div>
             <div className = "title_col">{name}</div>
-            <div className = "date_col">{new Date(createDate).toLocaleDateString()}</div>
+            <div className = "date_col">{new Date(createdDate).toLocaleDateString()}</div>
             <div className = "button_col">
                 <button>삭제</button>
             </div>
