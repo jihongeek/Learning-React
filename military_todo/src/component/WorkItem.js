85776c9342e815e1b555498d06b4810d269c36fd
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-07-02 09:48:44 +0000

    받은 Props를 가져와서 렌더링 해주기

--- military_todo/src/component/WorkItem.js
+++ military_todo/src/component/WorkItem.js
@@ -1,12 +1,12 @@
 import "./WorkItem.css"
-const WorkItem = () => {
+const WorkItem = ({id, name, isDone, createDate}) => {
     return (
         <div className = "WorkItem">
             <div className = "checkbox_col">
-                <input type = "checkbox" />
+                <input type = "checkbox" checked = {isDone}/>
             </div>
-            <div className = "title_col">전방에 힘찬 함성 3초간 실시!</div>
-            <div className = "date_col">{new Date().toLocaleDateString()}</div>
+            <div className = "title_col">{name}</div>
+            <div className = "date_col">{new Date(createDate).toLocaleDateString()}</div>
             <div className = "button_col">
                 <button>삭제</button>
             </div>
