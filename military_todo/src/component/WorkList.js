3d90a111b29c842bfbe25ef3ccc56e95cddcb5cb
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-07-03 14:35:57 +0000

    삭제함수를 Props에서 받아서 WorkItem Props에 넘겨주기

--- military_todo/src/component/WorkList.js
+++ military_todo/src/component/WorkList.js
@@ -1,14 +1,14 @@
 import "./WorkList.css"
 import WorkItem from "./WorkItem"
 
-const WorkList = ({work}) => {
+const WorkList = ({work,onDelete}) => {
     return (
       <div className = "WorkList">
           <p>작업목록표</p>
           <input className = "searchbar" placeholder = "작업 검색" ></input>
           <div className = "list_wrapper">
             {work.map((it) => (
-              <WorkItem {...it}/>
+              <WorkItem {...it} onDelete={onDelete}/>
             ))}
           </div>
       </div>  
