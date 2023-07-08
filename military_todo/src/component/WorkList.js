8b9727f8ed3ce15fc09d718307804bd0d3174c2c
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2023-07-08 08:38:59 +0000

    state를 이용해 검색기능 구축

--- military_todo/src/component/WorkList.js
+++ military_todo/src/component/WorkList.js
@@ -1,15 +1,25 @@
 import "./WorkList.css"
 import WorkItem from "./WorkItem"
+import {useState} from "react"
 
 const WorkList = ({work,onDelete}) => {
+    const [queryData,setQueryData] = useState("");
+    const onInputChange = (event) => {
+      setQueryData(event.target.value);      
+    }  
+
     return (
       <div className = "WorkList">
           <p>작업목록표</p>
-          <input className = "searchbar" placeholder = "작업 검색" ></input>
+          <input className = "searchbar" placeholder = "작업 검색" onChange={onInputChange} ></input>
           <div className = "list_wrapper">
-            {work.map((it) => (
-              <WorkItem key = {it.id} {...it} onDelete={onDelete}/>
-            ))}
+            {work.map((it) => {
+              if (it.name.includes(queryData))
+              {
+                return <WorkItem key = {it.id} {...it} onDelete={onDelete}/>;
+              } 
+              return null;
+            })}
           </div>
       </div>  
     );
